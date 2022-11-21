6103aa1d3a03fb29d62338d25c5c728b87dfca95
Live session user <mateus.smarques.silva@gmail.com>
2022-11-21 09:29:25 -0300

    first structure

---
+++ init.sh
@@ -0,0 +1,17 @@
+GIT_TOKEN="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"
+echo "install vim"
+sudo snap install nvim --classic
+echo "install brave"
+sudo snap install brave
+echo "insta git"
+sudo apt install git
+echo "install nodejs"
+sudo apt install nodejs
+echo "install nvm"
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+echo "nvm version to 16.14"
+nvm install 16.14
+echo "install yarn"
+npm install --global yarn
+git config --global user.email 'mateus.smarques.silva@gmail.com'
+git config --global user.name 'Mateus Sponton'
